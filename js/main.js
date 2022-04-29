1368c39283b460be9dbc99bd5363c9178a492deb
jordan727 <imjordanantonio@gmail.com>
2022-04-29 10:08:44 -0600

    Update Functions

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
 
 let timeLimit = 30;
 
+let wordText = document.querySelector("#words")
+
 let inputArea = document.getElementById("inputarea");
 
 let wordArray = [];
@@ -19,6 +21,13 @@ let lettersTyped = 0;
 let wordString = "";
 let timer = null;
 
+updateWords()
+document.addEventListener("keydown", startGame)
+
+function startGame() {
+    reset()
+    inputArea.focus()
+}
 
 // add 100 random words to array
 function updateWords() {
@@ -29,18 +38,60 @@ function updateWords() {
     } else if (wordArray.length >= 100) {
         console.log('worked')
     }
-    // Split word array
-    wordString = wordArray.join(" ");
-    document.getElementById("words").innerHTML = wordString
-}
 
+    // Word String
+    wordString = wordArray.join(" ");
+    wordString.split('').forEach(char => {
+        const charSpan = document.createElement('span')
+        charSpan.innerText = char
+        wordText.appendChild(charSpan)
+    })
+    document.getElementById("words").innerHTML = wordString
+
+    // Split word Array
+    splitWordArray = wordString.split('')
+}
 
 // turn typed text into array, split into letters, compare to random word array split into letters
 function matchText() {
     currentInput = inputArea.value;
     currentInputArray = currentInput.split('');
+
+    n = currentInputArray.length - 1;
+
+    wordSpanArray = wordText.querySelectorAll('span');
+    wordSpanArray.forEach((char, index) => {
+        let typedChar = currentInputArray[index]
+        if (currentInputArray[n] == splitWordArray[n]) {
+            console.log('nice');
+            char.classList.add('correct_char');
+            char.classList.remove('incorrect_char');
+        } else {
+            console.log('not nice');
+            char.classList.add('incorrect_char');
+            char.classList.remove('correct_char');
+        }
+    
+    })
     // compare split input and word arrays together and change colour of text depending if correct or not
     
+    // if (typedChar == null) {
+    //     char.classList.remove('correct_char');
+    //     char.classList.remove('incorrect_char');
+   
+        // correct character
+    //   } else if (typedChar === char.innerText) {
+    //     char.classList.add('correct_char');
+    //     char.classList.remove('incorrect_char');
+   
+        // incorrect character
+    //   } else {
+    //     char.classList.add('incorrect_char');
+    //     char.classList.remove('correct_char');
+   
+    //     // increment number of errors
+    //     errors++;
+    //   }
 }
 
 function updateTimer() {
@@ -48,11 +99,6 @@ function updateTimer() {
 }
 
 function endGame() {
-
-}
-
-function startGame() {
-    updateWords()
 }
 
 function reset() {
